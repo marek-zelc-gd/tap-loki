f7cd7233210f328d2f77b329a2b270b7fa16fecb
marek-zelc-gd <41433191+marek-zelc-gd@users.noreply.github.com>
2026-02-20 09:56:56 +0100

    Update __init__.py

--- tap_loki/__init__.py
+++ tap_loki/__init__.py
@@ -70,7 +70,7 @@ class CustomClient(Client):
 
         params['query'] = metric
         params['step'] = step
-        params['limit'] = 1000
+        params['limit'] = 5000
 
         if start is None:
             start = time_to_epoch((datetime.now() - timedelta(days=1)))
